69162cd971520f307fca3347ea3a689720e477e0
Edgar <epm_1998_eti@hotmail.com>
2021-02-10 12:02:07 -0600

    FEAT: new structure returned data Users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,19 +2,43 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req,res) => {
+    let response = {
+        "result":0,
+        "data": [],
+        "error": ""
+    }
     User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error:' + err));
+    .then(users => {
+        response["data"] = users;
+        response["result"] = 1;
+        res.json(response);
+    })
+    .catch(err => {
+        response["error"] = err;
+        res.status(400).json(response);
+    });
 });
 
 router.route('/add').post((req,res) => {
     const username = req.body.username;
-
     const newUser = new User({username});
-
+    let response = {
+        "result":0,
+        "data": {},
+        "error": ""
+    }
     newUser.save()
-    .then(() => res.json('user added!'))
-    .catch(err => res.status(400).json('Error:' + err));
+    .then(docInserted => {
+        response["data"] = {
+            "_id": docInserted["_id"]
+        }
+        response["result"] = 1
+        res.json(response)
+    })
+    .catch(err => {
+        response["error"] = err
+        res.status(400).json(response)
+    });
 });
 
 module.exports = router;
